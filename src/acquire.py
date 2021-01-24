60ac282523733f0ee50a4b5d96efa6da55a73a61
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-01-24 07:10:22 -0600

    LADOT Tweets

--- src/acquire.py
+++ src/acquire.py
@@ -58,9 +58,21 @@ def get_sweep_data():
 
 
 ###################### REST API Functions ######################
+def auth():
+    '''
+    Token to access Twitter's API
+    '''
+    return env.bearer_token
+
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
 def base_url():
     '''
-    Returns base url to acquire LinkedIn job posts.
+    Returns base url to acquire data from Twitter using RESTAPI.
     
     Parameters
     ----------
@@ -68,7 +80,7 @@ def base_url():
     
     Returns
     -------
-    string url to acquire LinkedIn data.
+    string url to acquire twitter data.
     '''
     return  "https://api.twitter.com/2/"
 
@@ -85,7 +97,7 @@ def response_endpoint(endpoint):
         
         Example
         -------
-        endpoint = "/documentation"
+        endpoint = "users"
         base_url = "https://api.twitter.com/2/"
         
     Returns
@@ -94,52 +106,6 @@ def response_endpoint(endpoint):
     '''
     get_request = requests.get(base_url() + endpoint)
     return get_request
-
-
-def max_page(url):
-    '''
-    Accepts a requests.models.Response object
-    
-    Return the maximum page for a specific endpoint
-    Parameters
-    ----------
-    url : requests.models.Response
-        A response from an endpoint using REST API
-    
-    Returns
-    -------
-    integer value
-    '''
-    return url.json()['payload']['max_page']
-
-
-def page_iterator(data, data_path, stop_page):
-    '''
-    Accepts an endpoint name, path to endpoint, and number of pages to acquire.
-    
-    Return a specific H-E-B dataset as a pandas DataFrame.
-    Parameters
-    ----------
-    data : str
-        The name of an endpoint to retrieve H-E-B data
-    
-    data_path : str
-        The path to the specified endpoint
-    
-    stop_page : int
-        The page number to stop on - inclusive.
-    
-    Returns
-    -------
-    pandas DataFrame
-    '''
-    df = pd.DataFrame()
-    
-    for page in range(1, stop_page+1):
-        response = requests.get(base_url() + data_path + '?page=' + str(page))
-        df = df.append(response.json()['payload'][f'{data}'])
-
-    return df
 
 
 def check_local_cache(data):
@@ -168,31 +134,6 @@ def check_local_cache(data):
     
     
 ############################# Acquire News Articles ######################################
-def check_local_cache(file_name):
-    '''
-    Accepts a filename and checks to see if a local
-    cached version of the data exists
-    
-    Returns endpoint data as a pandas DataFrame if a local cache exists
-    Returns False if a local cache does not exist.
-    
-    Parameters
-    ----------
-    file_name : str
-        
-    Returns
-    -------
-    Return cached file as a pandas DataFrame if : os.path.isfile(file_name) == True
-    Return False otherwise
-    '''
-    if os.path.isfile(file_name):
-        f = open(file_name)
-        data = json.load(f)
-        return pd.DataFrame(data)
-    else:
-        return False
-
-
 def url_request(url):
     '''
     Accepts a URL
